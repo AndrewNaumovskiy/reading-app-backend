b1d311a144a6638ea10626b372e1280551fd1263
Naumovskyi Andrii <32496284+AndrewNaumovskiy@users.noreply.github.com>
2024-06-11 16:53:47 +0300

    Add User endpoint. Add database connection

---
+++ ReadingApp/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using ReadingApp.Models;
+using ReadingApp.Helpers;
+using ReadingApp.Models.DbModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReadingApp.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDbContextFactory<ReadingDbContext> _dbContext;
+        public UsersController(IDbContextFactory<ReadingDbContext> dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [EnableCors]
+        [Route("{nickname}")]
+        public async Task<ActionResult<ResponseModel<GetUserData, IError>>> GetByNickname(string nickname)
+        {
+            UserDbModel user = null;
+
+            using (var db = await _dbContext.CreateDbContextAsync())
+            {
+                user = await db.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Nickname == nickname);
+
+                if (user == null)
+                    return Ok(new ResponseModel<IData, Error>()
+                    {
+                        Error = new Error("No user found")
+                    });
+            }
+
+            return Ok(new ResponseModel<GetUserData, IError>()
+            {
+                Data = new GetUserData(user)
+            });
+        }
+    }
+}
---
+++ ReadingApp/Helpers/ReadingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ReadingApp.Models.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReadingApp.Helpers
+{
+    public class ReadingDbContext : DbContext
+    {
+        public DbSet<UserDbModel> Users { get; set; }
+
+        public ReadingDbContext(DbContextOptions<ReadingDbContext> options) : base(options) { }
+    }
+}
